e8758bb3645bf1162ccbe0ac71fc09293ba737ac
Carlos Vera A <carveraa13@gmail.com>
2021-04-10 00:43:11 +0000

    update

--- src/models.py
+++ src/models.py
@@ -8,6 +8,13 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User')
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
@@ -16,35 +23,28 @@ class User(Base):
     lastname = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
 
-class Follower(Base):
-    __tablename__ = 'follower'
-    id = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey('user.id'))
-    user_to_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
     types = Column(Enum, nullable=False)
     url = Column(String(150), nullable=False)
     post_id = Column(Integer, ForeignKey('post.id'))
-    post = relationship(Post)
+    post = relationship('Post')
 
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship('User')
 
 class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(150), nullable=False)
     author_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship('User')
     post_id = Column(Integer, ForeignKey('post.id'))
-    post = relationship(Post)
+    post = relationship('Post')
  
     def to_dict(self):
         return {}
